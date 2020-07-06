05cc095bd21402b776fcfec9c92805c5a794b9c5
TensorFlow Recommenders Team <tensorflow-recommenders-team@google.com>
2020-07-06 11:39:17 -0700

    Internal change.
    PiperOrigin-RevId: 319821899

--- tensorflow_recommenders/__init__.py
+++ tensorflow_recommenders/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tensorflow recommenders."""
+"""TensorFlow Recommenders."""
 
 __version__ = "v0.1.0"
 

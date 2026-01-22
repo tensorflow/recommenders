cf1dafa988f223ed5f30b68cb9aed518484b3a1d
Maciej Kula <maciejkula@google.com>
2026-01-21 20:04:10 -0800

    Bump version.
    PiperOrigin-RevId: 859376966

--- tensorflow_recommenders/__init__.py
+++ tensorflow_recommenders/__init__.py
@@ -46,7 +46,7 @@ _check_keras_version()
 del _check_keras_version
 # pylint: enable=g-statement-before-imports
 
-__version__ = "v0.7.6"
+__version__ = "v0.7.7"
 
 from tensorflow_recommenders import examples
 from tensorflow_recommenders import experimental
